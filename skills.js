3f25a5b49a84078372c8c169122407149751c0c7
timmy wen <118405430+embracefailure@users.noreply.github.com>
2023-07-19 02:30:22 +0000

    Copilot first commit

---
+++ skills.js
@@ -0,0 +1,15 @@
+function calculateNumbers(var1, var2) {
+    return var1 + var2;
+}
+
+function multiplyNumbers(var1, var2) {
+    return var1 * var2;
+}
+
+function divideNumbers(var1, var2) {
+    return var1 / var2;
+}
+
+function subtractNumbers(var1, var2) {
+    return var1 - var2;
+}
